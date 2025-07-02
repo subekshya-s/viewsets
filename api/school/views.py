ade64cd64306529025b2308d1ca80b053bbd04bd
subekshya-s <subekshyasubedi26@gmail.com>
2025-07-02 13:31:53 +0545

    feat:pagination and sorting added

--- api/school/views.py
+++ api/school/views.py
@@ -1,64 +1,70 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status  
+from rest_framework import status, viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 from school.models import Student
 from school.serializer import StudentSerializer
-from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
 
-#adding the pagination
+# Custom pagination
 class StudentPagination(PageNumberPagination):
-      page_size=5
-      max_page_size=50
+    page_size = 5
+    max_page_size = 50
 
 class StudentViewSet(viewsets.ViewSet):
-    pagination_class=StudentPagination
+    pagination_class = StudentPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['name']             
+    ordering_fields = ['roll']           
+    filterset_fields = ['roll']          
 
     def list(self, request):
+        queryset = Student.objects.all()
+
+        # Applying filtering, search, and ordering
+        for backend in list(self.filter_backends):
+            queryset = backend().filter_queryset(request, queryset, self)
+
+        # Applying pagination
         paginator = self.pagination_class()
-        stu = Student.objects.all()
-        page = paginator.paginate_queryset(stu, request)
+        page = paginator.paginate_queryset(queryset, request)
+
         serializer = StudentSerializer(page, many=True)
         return paginator.get_paginated_response(serializer.data)
 
-    
-    def retrieve(self,request,pk=None):
-        id=pk
-        if id is not None:
-           stu=Student.objects.get(id=id)
-           serializer=StudentSerializer(stu)
-           return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        if pk is not None:
+            stu = Student.objects.get(id=pk)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
 
-    def create(self,request):
+    def create(self, request):
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
-           serializer.save()
-           return Response({'msg':'Data Created'},status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({'msg': 'Data Created'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self,request,pk):
-        id=pk
-        stu=Student.objects.get(pk=id)
-        serializer=StudentSerializer(stu, data=request.data)
+    def update(self, request, pk):
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':'Complete Data Updated'})
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response({'msg': 'Complete Data Updated'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def partial_update(self,request,pk):
-        id=pk
-        stu=Student.objects.get(pk=id)
-        serializer = StudentSerializer(stu,data=request.data,partial=True)
+    def partial_update(self, request, pk):
+        stu = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':'Partial Data Updated'})
+            return Response({'msg': 'Partial Data Updated'})
         return Response(serializer.errors)
 
-    def delete(self,request,pk):
-        id=pk
-        stu=Student.objects.get(pk=id)
+    def delete(self, request, pk):
+        stu = Student.objects.get(pk=pk)
         stu.delete()
-        return Response({'msg':'Data Deleted'})
+        return Response({'msg': 'Data Deleted'})
+
 
 
 
